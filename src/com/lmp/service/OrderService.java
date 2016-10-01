a542d74038ffe5d5edf76c3388186bb65a0d0595
Muhammad Farhan Anjum Pirzada <pirzada@live.com>
2016-10-01 01:23:41 -0500

    Order Service

---
+++ src/com/lmp/service/OrderService.java
@@ -0,0 +1,60 @@
+package com.lmp.model;
+
+import java.util.Date;
+
+public class OrderService {
+
+
+	private int customerID;
+	private int paymentID;
+	private int orderID;
+	private int orderStatus;
+	private String trackingNumber;
+	private Date datePurchased;
+	private Date dateRefunded;
+	
+	public Date getDatePurchased() {
+		return datePurchased;
+	}
+	public void setDatePurchased(Date datePurchased) {
+		this.datePurchased = datePurchased;
+	}
+	public Date getDateRefunded() {
+		return dateRefunded;
+	}
+	public void setDateRefunded(Date dateRefunded) {
+		this.dateRefunded = dateRefunded;
+	}
+	public int getCustomerID() {
+		return customerID;
+	}
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+	public int getPaymentID() {
+		return paymentID;
+	}
+	public void setPaymentID(int paymentID) {
+		this.paymentID = paymentID;
+	}
+	public int getOrderID() {
+		return orderID;
+	}
+	public void setOrderID(int orderID) {
+		this.orderID = orderID;
+	}
+	public int getOrderStatus() {
+		return orderStatus;
+	}
+	public void setOrderStatus(int orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	public String getTrackingNumber() {
+		return trackingNumber;
+	}
+	public void setTrackingNumber(String trackingNumber) {
+		this.trackingNumber = trackingNumber;
+	}
+	
+	
+}
