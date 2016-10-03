9cae48dbe2fa51f522c1f81aaa318f7e8d92ee96
Mike Giovenco <michael.e.giovenco@gmail.com>
2016-10-03 14:39:53 -0500

    Reformatting team's code

--- src/com/lmp/view/TestClient4Partner.java
+++ src/com/lmp/view/TestClient4Partner.java
@@ -15,11 +15,11 @@ public class TestClient4Partner {
         System.out.println("getPartner(1): " + partnerService.getPartner(1));
         System.out.println("listPartners: " + partnerService.listPartners());
 
-        Partner partner1 = new Partner("MYCO", "6331 N Kenmore Ave", "Chicago", "IL", “60660”, “USA”, ”abc@xyz.com”, ”xyz.com”, ”Check”, true);
+        Partner partner1 = new Partner("MYCO", "6331 N Kenmore Ave", "Chicago", "IL", "60660", "USA", "abc@xyz.com", "xyz.com", "Check", true);
         partnerService.addPartner(partner1);
         System.out.println("listPartners - after new partner add: " + partnerService.listPartners());
 
-        Partner partner2 = new Partner(1, “MYCOM", "6332 N Kenmore Ave", "Chicago", "IL", “60660”, “USA”, ”xyz@xyz.com”, ”xyz.com”, ”Cash”, true);
+        Partner partner2 = new Partner(1, "MYCOM", "6332 N Kenmore Ave", "Chicago", "IL", "60660", "USA", "xyz@xyz.com”, ”xyz.com", "Cash", true);
         partnerService.updatePartner(partner2);
         System.out.println("listPartners - after update: " + partnerService.listPartners());
 
