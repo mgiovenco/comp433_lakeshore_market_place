c00115b7fa522fd64b1327319fc8a58cc30440ce
Muhammad Farhan Anjum Pirzada <pirzada@live.com>
2016-10-03 02:06:59 -0500

    Add files via upload

---
+++ src/com/lmp/view/TestClient4Partner.java
@@ -0,0 +1,29 @@
+package com.lmp.view;
+
+import com.lmp.dao.PartnerDao;
+import com.lmp.model.Partner;
+import com.lmp.service.PartnerService;
+
+import java.math.BigDecimal;
+
+public class TestClient4Partner {
+
+    public static void main(String[] args) throws Exception {
+o
+        System.out.println("THIS IS PARTNER SERVICE TEST");
+        PartnerService partnerService = new PartnerService(new PartnerDao());
+        System.out.println("getPartner(1): " + partnerService.getPartner(1));
+        System.out.println("listPartners: " + partnerService.listPartners());
+
+        Partner partner1 = new Partner("MYCO", "6331 N Kenmore Ave", "Chicago", "IL", “60660”,“USA”,”abc@xyz.com”,”xyz.com”,”Check”,true);
+        partnerService.addPartner(partner1);
+        System.out.println("listPartners - after new partner add: " + partnerService.listPartners());
+
+        Partner partner2 = new Partner(1, “MYCO2", "6332 N Kenmore Ave", "Chicago", "IL", “60660”,“USA”,”xyz@xyz.com”,”xyz.com”,”Cash”,true);
+        partnerService.updatePartner(partner2);
+        System.out.println("listPartners - after update: " + partnerService.listPartners());
+
+        partnerService.deletePartner(1);
+        System.out.println("listPartners - after delete: " + partnerService.listPartners());
+    }
+}
