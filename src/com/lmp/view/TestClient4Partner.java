4f08545e67df08c4abd49638a3952fae2464e87c
Muhammad Farhan Anjum Pirzada <pirzada@live.com>
2016-10-03 15:03:33 -0500

    Update TestClient4Partner.java

--- src/com/lmp/view/TestClient4Partner.java
+++ src/com/lmp/view/TestClient4Partner.java
@@ -15,11 +15,11 @@ public class TestClient4Partner {
         System.out.println("getPartner(1): " + partnerService.getPartner(1));
         System.out.println("listPartners: " + partnerService.listPartners());
 
-        Partner partner1 = new Partner("MYCO", "6331 N Kenmore Ave", "Chicago", "IL", "60660", "USA", "abc@xyz.com", "xyz.com", "Check", true);
+        Partner partner1 = new Partner("Myco", "6331 N Kenmore Ave", "Chicago", "IL", "60660", "USA", "800-556-8876", "abc@xyz.com", "http://www.xyz.com", "", "", "", "", true);
         partnerService.addPartner(partner1);
         System.out.println("listPartners - after new partner add: " + partnerService.listPartners());
 
-        Partner partner2 = new Partner(1, "MYCOM", "6332 N Kenmore Ave", "Chicago", "IL", "60660", "USA", "xyz@xyz.com”, ”xyz.com", "Cash", true);
+        Partner partner2 = new Partner(1, "Myorg", "6332 N Kenmore Ave", "Chicago", "IL", "60660", "USA", "800-666-7776", "abc@abc.com", "http://www.abc.com", "", "", "", "", true);
         partnerService.updatePartner(partner2);
         System.out.println("listPartners - after update: " + partnerService.listPartners());
 
